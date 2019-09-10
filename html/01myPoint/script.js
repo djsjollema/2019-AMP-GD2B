2247f3570a6874213139dfe3bea2196e43c2ce70
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-09-10 14:06:15 +0200

    myPoint

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,13 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let A = new Point(new Vector2d(getRandom(width),getRandom(height)));
+A.draw(context);
+
+let B = new Point(new Vector2d(getRandom(width),getRandom(height)));
+B.draw(context)
+
+function getRandom(max){
+  return Math.floor(Math.random()*max);
+}
