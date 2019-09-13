5d88534528a764aef4410623bbc2f3ba7ea9820b
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-09-13 14:27:26 +0200

    02_02

--- html/02myArray/script.js
+++ html/02myArray/script.js
@@ -8,5 +8,24 @@ canvas.width = width;
 canvas.height = height;
 
 let point = new Point(new Vector2d(200,300),100);
+let mouseVector = new Vector2d(0,0);
+let difference = new Vector2d(0,0);
 
-point.draw(context);  
+
+point.draw(context);
+
+window.addEventListener('click',(evt)=>{
+  mouseVector.dx = evt.clientX;
+  mouseVector.dy = evt.clientY;
+  console.log(mouseVector);
+  console.log(point.position);
+  difference.differenceVector(point.position,mouseVector);
+  console.log(difference)
+  console.log(difference.magnitude);
+  if(difference.magnitude<= point.radius){
+    point.position.dx += 50;
+    point.position.dy += 50;
+    point.draw(context);
+  }
+
+})
