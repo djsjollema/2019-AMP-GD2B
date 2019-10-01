fbdb01ace174396514dc0fa80e797d82263f7355
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-10-01 13:53:55 +0200

    05_04

--- html/05drawTheLine/script.js
+++ html/05drawTheLine/script.js
@@ -7,16 +7,29 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(200,200),15);
-let B = new Point(new Vector2d(500,300),15);
+let A = new Point(new Vector2d(300,200),15,true);
+let B = new Point(new Vector2d(500,300),15,true);
 
-A.draw(context);
-B.draw(context);
+
 
 let l = new LinearFunction(1/3,133.3);
+l.defineLineByTwoPoints(A,B);
 
-for(let x = 0; x < width; x+= 10){
-  console.log(x,l.calcY(x));
-  let A = new Point(new Vector2d(x,l.calcY(x)),5);
+
+
+
+function animate(){
+  requestAnimationFrame(animate);
+  context.clearRect(0,0,width,height);
+  for(let x = 0; x < width; x+= 10){
+    let P = new Point(new Vector2d(x,l.calcY(x)),5);
+    P.draw(context);
+  }
+
   A.draw(context);
+  B.draw(context);
+
+
 }
+
+animate();
