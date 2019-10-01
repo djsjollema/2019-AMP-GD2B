8de1747863d2153666fe927c3b99dd9eda99d0cc
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-10-01 13:47:49 +0200

    05_03

--- html/05drawTheLine/script.js
+++ html/05drawTheLine/script.js
@@ -8,11 +8,15 @@ canvas.width = width;
 canvas.height = height;
 
 let A = new Point(new Vector2d(200,200),15);
+let B = new Point(new Vector2d(500,300),15);
 
 A.draw(context);
+B.draw(context);
 
-let l = new LinearFunction(1,100);
+let l = new LinearFunction(1/3,133.3);
 
-for(let x = 0; x < 100; x+= 10){
-  console.log(x,l.calcY(x))
+for(let x = 0; x < width; x+= 10){
+  console.log(x,l.calcY(x));
+  let A = new Point(new Vector2d(x,l.calcY(x)),5);
+  A.draw(context);
 }
