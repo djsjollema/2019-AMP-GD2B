71ef95cfd26548ffb5bbb5e52c9cbd660bdc84c7
djsjollema <j.sjollema@ma-web.nl>
2019-10-29 14:08:22 +0100

    11_03

--- html/11MousePointer/script.js
+++ html/11MousePointer/script.js
@@ -8,12 +8,14 @@ canvas.width = width;
 canvas.height = height;
 
 
-let arrow = new Arrow(new Vector2d(width/2,height/2));
-arrow.draw(context);
+let arrow = new Arrow(new Vector2d(width/2,height/2),0);
 
-// function animate(){
-//   requestAnimationFrame(animate);
-//   context.clearRect(0,0,width,height);
-// }
-//
-// animate();
+
+function animate(){
+  requestAnimationFrame(animate);
+  context.clearRect(0,0,width,height);
+  arrow.angle += 0.01;
+  arrow.draw(context);
+}
+
+animate();
