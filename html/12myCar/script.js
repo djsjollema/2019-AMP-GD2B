50d9a38a4d7b0491551849acfc280d6d2cc6b8d6
djsjollema <j.sjollema@ma-web.nl>
2019-11-05 13:54:27 +0100

    car --wheels added

--- html/12myCar/script.js
+++ html/12myCar/script.js
@@ -9,14 +9,39 @@ canvas.height = height;
 
 let grid = new Grid();
 
+let car = new Image();
+car.src = "img/car.png";
 
+car.pos = new Vector2d(0,0);
+car.vel = new Vector2d(7,0);
+
+let wheelFront = new Image();
+wheelFront.src = "img/wheel.png";
+wheelFront.pos = new Vector2d(0,0);
+
+
+car.addEventListener('load',()=>{
+  car.pos.dy = height - car.height;
+  animate();
+})
 
 function animate(){
+  context.clearRect(0,0,width,height);
   requestAnimationFrame(animate);
+  car.pos.add(car.vel);
   //context.clearRect(0,0,width,height);
   grid.draw(context);
+  context.drawImage(car,car.pos.dx,car.pos.dy);
+  wheelFront.pos.dx = car.pos.dx;
+  wheelFront.pos.dy = car.pos.dy;
+  wheelFront.pos.add(new Vector2d(673,123));
 
+  context.drawImage(wheelFront,wheelFront.pos.dx,wheelFront.pos.dy);
+  clamp();
 }
 
-
-animate();
+function clamp(){
+  if(car.pos.dx > width){
+    car.pos.dx = -car.width;
+  }
+}
