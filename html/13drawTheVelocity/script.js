cdcc46beb0b42734e22e204316268244f3e546c2
djsjollema <j.sjollema@ma-web.nl>
2019-11-19 13:33:29 +0100

    13 --dpoint

--- html/13drawTheVelocity/script.js
+++ html/13drawTheVelocity/script.js
@@ -7,10 +7,15 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
+let grid = new Grid();
+
+let point = new DPoint(new Vector2d(200,200),new Vector2d(7,8),new Vector2d(0,0),20,"white");
+
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
   grid.draw(context);
+  point.draw(context);
 }
 
 animate()
