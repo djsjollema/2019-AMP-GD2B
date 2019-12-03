a7d96bc2f880a0acfd348b0ceff8c952777f3b15
djsjollema <j.sjollema@ma-web.nl>
2019-12-03 13:57:06 +0100

    toAndFro --startcode

--- html/15dtoAndFro/script.js
+++ html/15dtoAndFro/script.js
@@ -14,6 +14,8 @@ let B = new Point(new Vector2d(500,400),20,true,"blue")
 
 let point = new DPoint(new Vector2d(200,300),new Vector2d(0,0),new Vector2d(0,0),10,"white");
 
+let toB = false; //true is to B, else to A
+
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
@@ -21,8 +23,12 @@ function animate(){
   A.draw(context);
   B.draw(context);
   point.draw(context);
-
-  point.vel.differenceVector(B.position,point.pos);
+  if(toB){
+    point.vel.differenceVector(B.position,point.pos);
+  } else {
+    point.vel.differenceVector(A.position,point.pos);
+  }
+  console.log(point.vel.magnitude)
   point.vel.scalarMul(0.01);
   point.update();
 
