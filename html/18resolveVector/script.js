ee70481361807b4cbca6d6884903a8eb7af61efb
djsjollema <j.sjollema@ma-web.nl>
2020-02-28 11:33:32 +0100

    18 --stap2

--- html/18resolveVector/script.js
+++ html/18resolveVector/script.js
@@ -9,8 +9,8 @@ canvas.height = height;
 
 let grid = new Grid();
 
-let A = new Point(new Vector2d(200,300),20,true,"red");
-let B = new Point(new Vector2d(500,400),20,true,"blue");
+let A = new Point(new Vector2d(200,300),5,true,"red");
+let B = new Point(new Vector2d(500,400),5,true,"blue");
 
 let C = new Point(new Vector2d(250,350),5,true,"yellow");
 
@@ -18,17 +18,24 @@ let C = new Point(new Vector2d(250,350),5,true,"yellow");
 let line_1 = new LinearFunction(1,1);
 let line_2 = new LinearFunction(1,1);
 
+let vector = new Vector2d(1,1);
+
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
   grid.draw(context);
+
+  vector.dx = B.position.dx - A.position.dx;
+  vector.dy = B.position.dy - A.position.dy;
 
   line_1.defineLineByTwoPoints(A,B);
   line_2.defineLineByTwoPoints(A,C);
 
   line_1.draw(context);
   line_2.draw(context);
-  
+
+  vector.draw(context,A.position,1)
+
   A.draw(context);
   B.draw(context);
 
